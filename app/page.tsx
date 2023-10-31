c98cd19827fc4edda14a70954d23cdd9452e810c
Augusto Cesar <augustocesarrezende64@gmail.com>
2023-10-31 17:10:45 -0300

    Starting working on Board component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Board from "@/components/Board";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main className="">
       <Header />
-      <h1>Heello Trello Clone</h1>
+      <Board />
     </main>
   );
 }
