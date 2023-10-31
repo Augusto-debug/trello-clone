65dca7e2c38605a35a4803f840a576bf19286066
Augusto Cesar <augustocesarrezende64@gmail.com>
2023-10-31 16:57:07 -0300

    Ui Gradient Done

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,7 @@ const Header = () => {
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center  p-5 bg-gray-500/10 rounded-b-2xl ">
+        <div className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 to-[#0055d1] -z-50 rounded-b-md filter blur-3xl opacity-50" />
         <Image
           src={"/images/trello_logo.png"}
           alt="trello logo"
