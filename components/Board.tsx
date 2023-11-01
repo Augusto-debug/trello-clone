86cb2856f6523c81f56919ea7333840effea23f7
Augusto Cesar <augustocesarrezende64@gmail.com>
2023-11-01 16:55:37 -0300

    Created a store folder to handle state management using zustand

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+"use client";
+import { useBoardStore } from "@/store/BoardStore";
+import { useEffect } from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 const Board = () => {
-  return <div>Board</div>;
+  const getBoard = useBoardStore((state) => state.getBoard);
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
+
+  return (
+    <h1>heeelo</h1>
+    // <DragDropContext>
+    //   <Droppable droppableId="board" direction="horizontal" type="column">
+    //     {(provided) => <div></div>}
+    //   </Droppable>
+    // </DragDropContext>
+  );
 };
 
 export default Board;
