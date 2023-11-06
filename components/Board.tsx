9335ce5b2613d10f3bd507b8b89e247da1a20056
Augusto Cesar <augustocesarrezende64@gmail.com>
2023-11-06 17:28:16 -0300

    Removed the border of the div

--- components/Board.tsx
+++ components/Board.tsx
@@ -25,7 +25,7 @@ const Board = () => {
           <div
             {...provided.droppableProps}
             ref={provided.innerRef}
-            className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto border-2 border-red-600"
+            className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
           >
             {Array.from(board.columns.entries()).map(([id, column], index) => (
               <Column key={id} id={id} todos={column.todos} index={index} />
