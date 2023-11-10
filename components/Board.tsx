862c1a690eb577c531f8a58940b8f12cd6245865
Augusto Cesar <augustocesarrezende64@gmail.com>
2023-11-10 13:44:12 -0300

    Drag and Drop Working

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useBoardStore } from "@/store/BoardStore";
 import { useEffect } from "react";
 import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
@@ -17,7 +16,6 @@ const Board = () => {
 
   const handleOnDragEnd = (result: DropResult) => {
     const { destination, source, type } = result;
-    console.log(destination, source, type);
 
     if (!destination) return;
 
@@ -27,6 +25,46 @@ const Board = () => {
       entries.splice(destination.index, 0, removed);
       const rearrangedColumns = new Map(entries);
       setBoardState({ ...board, columns: rearrangedColumns });
+    }
+    const columns = Array.from(board.columns);
+    const startColIndex = columns[Number(source.droppableId)];
+    const finishColIndex = columns[Number(destination.droppableId)];
+
+    const startCol: Column = {
+      id: startColIndex[0],
+      todos: startColIndex[1].todos,
+    };
+
+    const finishCol: Column = {
+      id: finishColIndex[0],
+      todos: finishColIndex[1].todos,
+    };
+
+    if (!startCol.id || !finishCol.id) return;
+    if (source.index === destination.index && startCol === finishCol) return;
+
+    const newTodos = startCol.todos;
+    const [todoMoved] = newTodos.splice(source.index, 1);
+
+    if (startCol.id === finishCol.id) {
+      newTodos.splice(destination.index, 0, todoMoved);
+      const newCol = { id: startCol.id, todos: newTodos };
+      const newColumns = new Map(board.columns);
+      newColumns.set(startCol.id, newCol);
+      setBoardState({ ...board, columns: newColumns });
+    } else {
+      const finishTodos = Array.from(finishCol.todos);
+      finishTodos.splice(destination.index, 0, todoMoved);
+      const newColumns = new Map(board.columns);
+      const newCol = { id: startCol.id, todos: newTodos };
+
+      newColumns.set(startCol.id, newCol);
+      newColumns.set(finishCol.id, {
+        id: finishCol.id,
+        todos: finishTodos,
+      });
+
+      setBoardState({ ...board, columns: newColumns });
     }
   };
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -14,6 +14,7 @@ const Header = () => {
           width={300}
           height={100}
           className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
+          priority
         />
         <div className="flex items-center space-x-5 flex-1 justify-end w-full">
           <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
