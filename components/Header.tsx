6b11eac50955dc111e56b90f4a0d825bd1c938dd
Augusto Cesar <augustocesarrezende64@gmail.com>
2023-11-14 17:44:12 -0300

    Search Working

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 import React from "react";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Avatar from "react-avatar";
+import { useBoardStore } from "@/store/BoardStore";
+import { stat } from "fs";
 const Header = () => {
+  const [searchString, setSearchString] = useBoardStore((state) => [
+    state.searchString,
+    state.setSearchString,
+  ]);
+
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center  p-5 bg-gray-500/10 rounded-b-2xl ">
@@ -22,6 +29,8 @@ const Header = () => {
             <input
               type="text"
               placeholder="Search"
+              value={searchString}
+              onChange={(e) => setSearchString(e.target.value)}
               className="outline-none p-2"
             />
             <button type="submit" hidden />
